0e479cb9b38e2231013a75b8ed42a07eb881c268
Raul Leon <planaileen1@gmail.com>
2026-03-09 02:51:32 -0500

    cambios importantes 1.1

--- app/consejero/page.tsx
+++ app/consejero/page.tsx
@@ -1,18 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { useSearchParams, useRouter } from "next/navigation";
-
 export default function ConsejeroPage() {
-  const searchParams = useSearchParams();
-  const router = useRouter();
-  const consejeroId = searchParams.get("consejeroId");
-
-  useEffect(() => {
-    if (consejeroId) {
-      router.replace(`/consejero/${consejeroId}`);
-    }
-  }, [consejeroId, router]);
-
   return null;
 }
