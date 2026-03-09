49d8a7c401cb08c831e047cddb603a853b916b5a
Raul Leon <planaileen1@gmail.com>
2026-03-09 18:46:47 -0500

    nuevo landing 5

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCyG_xX61vgLwyKHwPInYlD0jyl2Vb7HPk",
-  authDomain: "conquistadores-a5ee3.firebaseapp.com",
-  projectId: "conquistadores-a5ee3",
-  storageBucket: "conquistadores-a5ee3.firebasestorage.app",
-  messagingSenderId: "654512522918",
-  appId: "1:654512522918:web:460b6355aa074caf43d667"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
