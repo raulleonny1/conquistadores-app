57ba1079dba80fe376bbbc7ba92a4060c471aa7f
Raul Leon <planaileen1@gmail.com>
2026-03-11 09:20:03 -0500

    aspirante guia mayor 2

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -11,8 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const db = getFirestore(app);
+export const storage = getStorage(app);
 // Formatea una fecha JS a dd/mm/yyyy con ceros
 export function formatFechaDDMMYYYY(date: Date | string): string {
   let d: Date;
