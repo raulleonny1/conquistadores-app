844bc2b7683383d19b009f5bba15d77d3b6b025e
Raul Leon <planaileen1@gmail.com>
2026-03-14 00:51:19 -0500

    mejoras importantes 5

--- app/conquistadores/calificaciones/page.tsx
+++ app/conquistadores/calificaciones/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import { db } from "../../../src/firebase";
 import { collection, doc, onSnapshot, setDoc } from "firebase/firestore";
